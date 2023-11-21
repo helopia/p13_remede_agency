c28446e519aaaaab0bfe7d76f6d259005a454341
helopia <75479527+helopia@users.noreply.github.com>
2023-11-21 23:51:30 +0100

    rep

--- website/src/components/layout/Header.jsx
+++ website/src/components/layout/Header.jsx
@@ -1,4 +1,4 @@
-import Logo from "../assets/images/argentBankLogo.png"
+import Logo from "../../assets/images/argentBankLogo.png"
 import {Link} from "react-router-dom";
 
 
--- website/src/router/router.js
+++ website/src/router/router.js
@@ -3,10 +3,6 @@ import Layout from "../components/layout/Layout";
 import Home from "../pages/Home";
 import User from "../pages/User";
 import Signin from "../pages/Signin";
-
-
-
-
 export const router = createBrowserRouter([
     {
         path: "/",
